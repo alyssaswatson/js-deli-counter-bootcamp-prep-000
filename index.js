6f64c7497d5412ad7962b1efac983477dc1bbf8b
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-12 00:08:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   var position = katzDeliLine.length + 1
   katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + position + "in line."
+  return "Welcome, " + name + ". You are number " + position + " in line."
 }

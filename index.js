a02cd7844f2f4258fcca0a644972be6bd4b2498a
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-12 00:12:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
   return "Welcome, " + name + ". You are number " + position + " in line."
 }
 function nowServing(deliLine) {
-  if(deliLine.lenght == 0)
+  if(deliLine.length == 0)
   return "There is nobody waiting to be served!"
 
 }

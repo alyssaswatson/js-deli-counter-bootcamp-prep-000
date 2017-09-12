28e35c0cc61e9435c99eec0a993f5ec579cbf665
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-12 00:14:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function takeANumber(katzDeliLine, name) {
   return "Welcome, " + name + ". You are number " + position + " in line."
 }
 function nowServing(deliLine) {
+  var name;
   if(deliLine.length == 0)
   return "There is nobody waiting to be served!"
   name = deliLine.shift()

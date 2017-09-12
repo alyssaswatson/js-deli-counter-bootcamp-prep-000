78e443f12619cd9f9219da2ead46af01a2884801
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-12 00:25:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(deliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  if(katzDeliLine.lenght == 0){
+  if(katzDeliLine.length == 0){
     return "The line is currently empty."
   }
   var myString = `The line is currently: 1. ${katzDeliLine[0]}`
